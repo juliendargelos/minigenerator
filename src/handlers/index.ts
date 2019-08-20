9fcbf9180310f6b1143ed522f873ed6f425a9b4b
Julien Dargelos <juliendargelos@users.noreply.github.com>
2019-08-20 15:09:52 +0200

    Update index.ts

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,3 +1,5 @@
 export { default as ejs } from './ejs'
 export { default as removeExtension } from './remove-extension'
 export { default as rename } from './rename'
+export { default as only } from './only'
+export { default as except } from './except'
